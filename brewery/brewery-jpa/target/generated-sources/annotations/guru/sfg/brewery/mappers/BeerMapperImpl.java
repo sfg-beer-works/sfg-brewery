0b9b7ecbca2f6c2dc2bc63b64adb6bccb53d7c5a
John Thompson <john@springframework.guru>
2019-02-13 09:20:00 -0500

    refactoring module layout

---
+++ brewery/brewery-jpa/target/generated-sources/annotations/guru/sfg/brewery/mappers/BeerMapperImpl.java
@@ -0,0 +1,57 @@
+package guru.sfg.brewery.mappers;
+
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.domain.Beer.BeerBuilder;
+import guru.sfg.brewery.model.BeerDto;
+import guru.sfg.brewery.model.BeerDto.BeerDtoBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2019-02-13T09:19:04-0500",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11 (Oracle Corporation)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.beerName( beer.getBeerName() );
+        beerDto.beerStyle( beer.getBeerStyle() );
+        beerDto.price( beer.getPrice() );
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        if ( beerDto == null ) {
+            return null;
+        }
+
+        BeerBuilder beer = Beer.builder();
+
+        beer.id( beerDto.getId() );
+        if ( beerDto.getVersion() != null ) {
+            beer.version( beerDto.getVersion().longValue() );
+        }
+        beer.createdDate( dateMapper.asTimestamp( beerDto.getCreatedDate() ) );
+        beer.lastModifiedDate( dateMapper.asTimestamp( beerDto.getLastModifiedDate() ) );
+        beer.beerName( beerDto.getBeerName() );
+        beer.beerStyle( beerDto.getBeerStyle() );
+        beer.price( beerDto.getPrice() );
+
+        return beer.build();
+    }
+}
