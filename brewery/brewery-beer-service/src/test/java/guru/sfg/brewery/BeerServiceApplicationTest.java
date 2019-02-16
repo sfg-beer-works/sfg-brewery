deafe7249c807bc680e4fd136b95849552ea5c46
John Thompson <john@springframework.guru>
2019-02-16 12:38:37 -0500

    adding context test, renamed mainclass

--- brewery/brewery-beer-service/src/test/java/guru/sfg/brewery/BeerServiceApplicationTest.java
+++ brewery/brewery-beer-service/src/test/java/guru/sfg/brewery/BeerServiceApplicationTest.java
@@ -17,12 +17,13 @@
 
 package guru.sfg.brewery;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class BreweryApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BreweryApplication.class, args);
+@SpringBootTest
+class BeerServiceApplicationTest {
+
+    @Test
+    void testContextLoads() {
     }
 }
