0b38bbae4354d304d48b6c173ba2ac7e4e6011d6
John <john@springframework.guru>
2019-03-05 13:25:42 -0500

    adding wiremock test for callback event & implemented callback

--- brewery/brewery-order-service/src/main/java/guru/sfg/brewery/events/BeerOrderStatusChangeEventListener.java
+++ brewery/brewery-order-service/src/main/java/guru/sfg/brewery/events/BeerOrderStatusChangeEventListener.java
@@ -17,17 +17,52 @@
 
 package guru.sfg.brewery.events;
 
+import guru.sfg.brewery.web.mappers.DateMapper;
+import guru.sfg.brewery.web.model.OrderStatusUpdate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Component
 public class BeerOrderStatusChangeEventListener {
+
+    RestTemplate restTemplate;
+    DateMapper dateMapper = new DateMapper();
+
+    public BeerOrderStatusChangeEventListener(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
     @Async
     @EventListener
     public void listen(BeerOrderStatusChangeEvent event){
         System.out.println("I got an order status change event");
         System.out.println(event);
+
+        OrderStatusUpdate update = OrderStatusUpdate.builder()
+                .id(event.getBeerOrder().getId())
+                .orderId(event.getBeerOrder().getId())
+                .version(event.getBeerOrder().getVersion() != null ? event.getBeerOrder().getVersion().intValue() : null)
+                .createdDate(dateMapper.asOffsetDateTime(event.getBeerOrder().getCreatedDate()))
+                .lastModifiedDate(dateMapper.asOffsetDateTime(event.getBeerOrder().getLastModifiedDate()))
+                .orderStatus(event.getBeerOrder().getOrderStatus().toString())
+                .customerRef(event.getBeerOrder().getCustomerRef())
+                .build();
+
+        try{
+            log.debug("Posting to callback url");
+            System.out.println("####################");
+            System.out.println("####################");
+            System.out.println("####################");
+            System.out.println("####################");
+            System.out.println("####################");
+            restTemplate.postForObject(event.getBeerOrder().getOrderStatusCallbackUrl(), update, String.class);
+        } catch (Throwable t){
+            log.error("Error Preforming callback for order: " + event.getBeerOrder().getId(), t);
+        }
     }
 }
