8e2c17906017c2dda6dbd384ab55fa749ff854ea
ximanta <ximanta.sarma#gmail.com>
2019-04-01 11:33:04 +0530

    Added CRUD for Customer

--- brewery/brewery-jpa/src/main/java/guru/sfg/brewery/repositories/CustomerRepository.java
+++ brewery/brewery-jpa/src/main/java/guru/sfg/brewery/repositories/CustomerRepository.java
@@ -19,10 +19,12 @@ package guru.sfg.brewery.repositories;
 import guru.sfg.brewery.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Created by jt on 2019-01-26.
  */
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    List<Customer> findAllByCustomerNameLike(String customerName);
 }
